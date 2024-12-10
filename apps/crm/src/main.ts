0381ccdf8edbc360195ec12eee2b180130d1939f
Thales Souza <santos.thales89@gmail.com>
2024-12-10 13:47:12 -0300

    enviroment variable aws

--- apps/crm/src/main.ts
+++ apps/crm/src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
   const ddb = new dynamoose.aws.ddb.DynamoDB({
     credentials: {
       accessKeyId: process?.env?.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process?.env?.AWS_ACCESS_SECRET
+      secretAccessKey: process?.env?.AWS_SECRET_ACCESS_KEY
     },
     region: process?.env?.AWS_REGION
   });
