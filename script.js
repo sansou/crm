60a2a83a1cfa624b9d3a962c2140edee5ad73e84
Thales Souza <santos.thales89@gmail.com>
2024-12-10 18:00:50 -0300

    FEAT- update script

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-function captureLead() {
-  const name = document.querySelector('#name').value;
-  const email = document.querySelector('#email').value;
-  const phone = document.querySelector('#phone').value;
-
-  if (!name || !email || !phone) {
-    console.log('Por favor, preencha todos os campos!');
-    return;
-  }
-
-  const lead = {
-    name: name,
-    email: email,
-    phone: phone,
-  };
-
-  console.log('Lead Capturado:', lead);
-
-  sendLeadToAPI(lead);
-}
-
 // Função para enviar os dados capturados para a API
-function sendLeadToAPI(lead) {
+function sendDataFormToAPI(formData) {
   fetch('http://localhost:3000/queue', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(lead),
+    body: JSON.stringify(formData),
   })
     .then(response => response.json())
     .then(data => {
-      console.log('Lead enviado com sucesso:', data);
+      console.log('Dados enviados com sucesso:', formData);
     })
     .catch(error => {
       console.error('Erro ao enviar o lead:', error);
@@ -38,7 +17,31 @@ function sendLeadToAPI(lead) {
 }
 
 // Adicionar um listener de evento para capturar o lead quando o formulário for enviado
-document.querySelector('#lead-form').addEventListener('submit', function (event) {
-  event.preventDefault();  // Impede o envio padrão do formulário
-  captureLead();
-});
+function captureLead() {
+  document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();  // Impede o envio padrão do formulário
+    const form = event.target;
+    const formData = new FormData(form);
+    sendDataFormToAPI(formData);
+  });
+}
+function getProjectId() {
+  document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();  // Impede o envio padrão do formulário
+    const form = event.target;
+    const formData = new FormData(form);
+    sendDataFormToAPI(formData);
+  });
+}
+
+async function main() {
+  captureLead()
+}
+
+(async () => {
+  try {
+    await main()
+  } catch (error) {
+    console.log("Erro", error);
+  }
+})()
