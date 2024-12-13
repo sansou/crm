dabfa46b5081112a432154a8115c7ac9f5dd9ece
Thales Souza <santos.thales89@gmail.com>
2024-12-13 14:47:45 -0300

    update entity

--- apps/crm/src/lead/entities/leads.entity.ts
+++ apps/crm/src/lead/entities/leads.entity.ts
@@ -1,10 +1,13 @@
 import { Item } from "dynamoose/dist/Item";
+import { StatusLead } from "../../utils/enums";
 
 export interface Lead extends Item{
   primaryKey: string,
   sortKey: string, // será o email
   name: string,
   phone: string,
+  status: StatusLead,
+  host: string,
   position?: string,
   state?: string,
   city?: string,
